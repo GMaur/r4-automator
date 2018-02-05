ed91d89041f7aa02561e1d56fb737a2c25ed0071
Alvaro Garcia <derecerca@gmail.com>
2018-02-05 21:14:45 +0100

    Add project with dependencies

---
+++ src/main/kotlin/com/gmaur/investment/r4automator/R4AutomatorApplication.kt
@@ -0,0 +1,11 @@
+package com.gmaur.investment.r4automator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class R4AutomatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<R4AutomatorApplication>(*args)
+}
