6a42d0df28b61a62e5e4d7b0c446eda31416e26c
Alvaro Garcia <derecerca@gmail.com>
2018-02-07 02:20:39 +0100

    Connect the 2FA PageObject and the Funds PageObject

--- src/main/kotlin/com/gmaur/investment/r4automator/R4AutomatorApplication.kt
+++ src/main/kotlin/com/gmaur/investment/r4automator/R4AutomatorApplication.kt
@@ -1,11 +1,17 @@
 package com.gmaur.investment.r4automator
 
+import com.gmaur.investment.infrastructure.funds.FundsConfiguration
+import com.gmaur.investment.infrastructure.funds.FundsPage
 import com.gmaur.investment.infrastructure.login.LoginConfiguration
 import com.gmaur.investment.infrastructure.login.LoginPage
+import com.gmaur.investment.infrastructure.twofactorauth.ConsoleTwoFactorAuthenticationProvider
+import com.gmaur.investment.infrastructure.twofactorauth.TwoFactorAuthenticationPage
 import org.openqa.selenium.WebDriver
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 
 @SpringBootApplication(scanBasePackages = arrayOf("com.gmaur.investment.infrastructure"))
@@ -14,16 +20,29 @@ class R4AutomatorApplication {
 
     var driver: WebDriver
 
-    constructor(driver: WebDriver, loginConfiguration: LoginConfiguration) {
+    private val `2faProvider` = ConsoleTwoFactorAuthenticationProvider(BufferedReader(InputStreamReader(System.`in`)), System.out)
+
+    constructor(driver: WebDriver, fundsConfiguration: FundsConfiguration, loginConfiguration: LoginConfiguration) {
         this.driver = driver
 
-        doWork(loginConfiguration)
-
+        logIn(loginConfiguration)
+        enable2FA()
+        parseFunds(fundsConfiguration)
+        
         this.driver.close()
     }
 
-    private fun doWork(loginConfiguration: LoginConfiguration) {
-        LoginPage(driver).login(loginConfiguration)
+    private fun parseFunds(fundsConfiguration: FundsConfiguration): String {
+        var funds = FundsPage(this.driver, fundsConfiguration).parse()
+        return funds
+    }
+
+    private fun enable2FA() {
+        TwoFactorAuthenticationPage(this.driver).enable(`2faProvider`)
+    }
+
+    private fun logIn(loginConfiguration: LoginConfiguration) {
+        LoginPage(this.driver).login(loginConfiguration)
     }
 
 }
