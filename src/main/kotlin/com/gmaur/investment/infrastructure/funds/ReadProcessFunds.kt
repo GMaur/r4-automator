dec85a6702faf668f95e7314e3d0ef8fb6766355
Alvaro Garcia <derecerca@gmail.com>
2018-02-07 04:35:23 +0100

    Refactor: extracted explaining method

--- src/main/kotlin/com/gmaur/investment/infrastructure/funds/ReadProcessFunds.kt
+++ src/main/kotlin/com/gmaur/investment/infrastructure/funds/ReadProcessFunds.kt
@@ -1,6 +1,7 @@
 package com.gmaur.investment.infrastructure.funds
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 import java.math.BigDecimal
 
 
@@ -9,11 +10,12 @@ class ParseFunds(val rawHtml: String) {
     fun run(): List<AssetDTO> {
         val doc = Jsoup.parse(rawHtml, "http://r4.com/")
         val assetsTable = doc.select(".tablemorning.table > tbody")[0]
+        fun isin(element: Element) = element.attr("data-isin")
 
         var elements = assetsTable.children()
-                .filter { element -> element.attr("data-isin") != "" }
+                .filter { element -> isin(element) != "" }
                 .map { element ->
-                    val isin = ISIN(element.attr("data-isin"))
+                    val isin = ISIN(isin(element))
                     var valueElement = element.getElementsByTag("td").flatMap { it -> it.getElementsByClass("fndVal_CNT_PATRIMONIO") }.first()
 
                     val value = valueElement.attr("data-fndval")
@@ -22,6 +24,7 @@ class ParseFunds(val rawHtml: String) {
 
         return elements
     }
+
 
     data class ISIN(val value: String)
 
