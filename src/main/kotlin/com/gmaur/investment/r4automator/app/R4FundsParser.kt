f49bc6abf3ffb91799689d73876c17e87cfbb505
Alvaro Garcia <derecerca@gmail.com>
2018-02-13 00:05:28 +0100

    #11 - Refactor: extract explaining methods

--- src/main/kotlin/com/gmaur/investment/r4automator/app/R4FundsParser.kt
+++ src/main/kotlin/com/gmaur/investment/r4automator/app/R4FundsParser.kt
@@ -4,6 +4,7 @@ import com.gmaur.investment.r4automator.infrastructure.files.FilePickerProvider
 import com.gmaur.investment.r4automator.infrastructure.files.FileUtils
 import com.gmaur.investment.r4automator.infrastructure.funds.FilePortfolioRepository
 import com.gmaur.investment.r4automator.infrastructure.funds.ParseFunds
+import com.gmaur.investment.r4automator.infrastructure.portfolio.Portfolio
 import java.io.File
 import java.nio.file.Paths
 
@@ -13,10 +14,20 @@ class R4FundsParser(private val portfolioRepo: FilePortfolioRepository) {
 
     fun run(args: Array<String>) {
         val file = filePickerProvider.request()
-        val portfolio = ParseFunds(FileUtils.readAllLinesAsString(Paths.get(file))).run()
+        val portfolio = parsePortfolio(file)
+        val output = savePortfolio(portfolio)
+        println("printed to file " + output)
+    }
+
+    private fun savePortfolio(portfolio: Portfolio): File? {
         val output = File.createTempFile("temp", "tmp")
         portfolioRepo.save(portfolio, output)
-        println("printed to file " + output)
+        return output
+    }
+
+    private fun parsePortfolio(file: String): Portfolio {
+        val portfolio = ParseFunds(FileUtils.readAllLinesAsString(Paths.get(file))).run()
+        return portfolio
     }
 
 }
