d62f28af97e7f6fb480339aa10d10f0e4b592615
Alvaro Garcia <derecerca@gmail.com>
2018-02-13 00:02:03 +0100

    #11 - Write the Funds JSON to file

--- src/main/kotlin/com/gmaur/investment/r4automator/app/R4FundsParser.kt
+++ src/main/kotlin/com/gmaur/investment/r4automator/app/R4FundsParser.kt
@@ -1,23 +1,41 @@
 package com.gmaur.investment.r4automator.app
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.gmaur.investment.r4automator.infrastructure.files.FilePickerProvider
 import com.gmaur.investment.r4automator.infrastructure.files.FileUtils
 import com.gmaur.investment.r4automator.infrastructure.funds.ParseFunds
+import com.gmaur.investment.r4automator.infrastructure.portfolio.Portfolio
+import java.io.File
 import java.nio.file.Paths
 
-class R4FundsParser {
+class R4FundsParser(private val portfolioRepo: FilePortfolioRepository) {
 
     private val filePickerProvider: FilePickerProvider = FilePickerProvider.aNew()
 
     fun run(args: Array<String>) {
-
         val file = filePickerProvider.request()
         val portfolio = ParseFunds(FileUtils.readAllLinesAsString(Paths.get(file))).run()
-        println(portfolio)
+        val output = File.createTempFile("temp", "tmp")
+        portfolioRepo.save(portfolio, output)
+        println("printed to file " + output)
     }
+
+}
+fun main(args: Array<String>) {
+    R4FundsParser(FilePortfolioRepository()).run(args)
 }
 
 
-fun main(args: Array<String>) {
-    R4FundsParser().run(args)
+class FilePortfolioRepository {
+
+    private var mapper: ObjectMapper = ObjectMapper().registerKotlinModule()
+
+
+    fun save(portfolio: Portfolio, file: File) {
+        mapper.enable(SerializationFeature.INDENT_OUTPUT)
+        mapper.writeValue(file, portfolio)
+    }
+
 }
