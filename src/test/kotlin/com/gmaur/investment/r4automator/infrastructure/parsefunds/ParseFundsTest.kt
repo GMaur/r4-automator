bcfe6fadcf233dd0c57c914043bf23ed1cd78c78
Alvaro Garcia <derecerca@gmail.com>
2018-02-12 23:05:47 +0100

    #11 Spike - serialize and deserialize Assets

--- src/test/kotlin/com/gmaur/investment/r4automator/infrastructure/parsefunds/ParseFundsTest.kt
+++ src/test/kotlin/com/gmaur/investment/r4automator/infrastructure/parsefunds/ParseFundsTest.kt
@@ -1,5 +1,9 @@
 package com.gmaur.investment.r4automator.infrastructure.parsefunds
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.gmaur.investment.r4automator.domain.Asset
 import com.gmaur.investment.r4automator.infrastructure.files.FileUtils
 import com.gmaur.investment.r4automator.infrastructure.funds.ParseFunds
 import com.gmaur.investment.r4automator.infrastructure.parsefunds.FundsObjectMother.funds_sample_1
@@ -9,6 +13,7 @@ import java.nio.file.Paths
 
 class ParseFundsTest {
 
+    val mapper = ObjectMapper().registerKotlinModule()
 
     @Test
     fun `parse the funds from the full sample`() {
@@ -24,6 +29,13 @@ class ParseFundsTest {
         var funds = ParseFunds(rawHtml).run()
 
         assertThat(funds).containsSequence(funds_sample_1())
+    }
+
+    @Test
+    fun `serialize`() {
+        val serialized = mapper.writeValueAsString(funds_sample_1())
+        val objects = mapper.readValue<List<Asset>>(serialized)
+        assertThat(objects).isEqualTo(funds_sample_1())
     }
 
     private fun readContents(file: String): String {
