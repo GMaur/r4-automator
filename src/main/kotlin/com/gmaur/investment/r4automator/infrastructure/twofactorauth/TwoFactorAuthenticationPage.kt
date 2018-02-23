a413c5c14f53f2426b6c50affe38702e225408d3
Alvaro Garcia <derecerca@gmail.com>
2018-02-23 02:31:40 +0100

    #28 - Only apply 2FA when needed

--- src/main/kotlin/com/gmaur/investment/r4automator/infrastructure/twofactorauth/TwoFactorAuthenticationPage.kt
+++ src/main/kotlin/com/gmaur/investment/r4automator/infrastructure/twofactorauth/TwoFactorAuthenticationPage.kt
@@ -9,14 +9,24 @@ class TwoFactorAuthenticationPage(private val driver: WebDriver) {
         PageFactory.initElements(driver, this)
     }
 
-    fun enable(provider: TwoFactorAuthenticationProvider) {
-        open2FAModal()
-        val code = provider.request()
-        insertSecondFactor(code)
-        submitForm()
-        //assert color is green
-        // driver.findElement(By.id("2fa")).findElement(By.tagName("img")).getAttribute("src")
-        //assert to "https://www.r4.com/recursos/images/mw-images/icon-2fa-operativa.png-operativa.png"
+    fun enable(provider: TwoFactorAuthenticationProvider): TwoFactorAuthenticationPage {
+        val twofaImage = fun(): String { return driver.findElement(By.id("2fa")).findElement(By.tagName("img")).getAttribute("src") }
+        if (twofaImage().contains("-consultiva.png")) {
+            open2FAModal()
+            val code = provider.request()
+            insertSecondFactor(code)
+            submitForm()
+            println("new url = " + twofaImage())
+            if (twofaImage().contains("-operativa.png")) {
+                println("2FA has been activated")
+            } else {
+                println("2FA could not be activated")
+            }
+        } else {
+            println("2FA is apparently enabled already")
+        }
+        return this
+    }
 
     fun pageSource(): String {
         return driver.pageSource
