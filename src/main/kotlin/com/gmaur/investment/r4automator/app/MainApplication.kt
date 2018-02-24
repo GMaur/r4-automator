f31b62b376f0c2c1b47bc8d12b1a1b67360e40f3
Alvaro Garcia <derecerca@gmail.com>
2018-02-24 12:00:28 +0100

    #30 - Only execute in debug mode

--- src/main/kotlin/com/gmaur/investment/r4automator/app/MainApplication.kt
+++ src/main/kotlin/com/gmaur/investment/r4automator/app/MainApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import kotlin.system.exitProcess
 
 @Configuration
 @EnableAutoConfiguration
@@ -14,5 +15,13 @@ import org.springframework.context.annotation.Import
 class MainApplication
 
 fun main(args: Array<String>) {
+    if (!inDebugMode()) {
+        println("This application is in alpha mode - please execute it in debug mode only")
+        exitProcess(1)
+    }
     SpringApplication.run(MainApplication::class.java, *args)
+}
+
+fun inDebugMode(): Boolean {
+    return java.lang.management.ManagementFactory.getRuntimeMXBean().inputArguments.toString().contains("-agentlib:jdwp")
 }
